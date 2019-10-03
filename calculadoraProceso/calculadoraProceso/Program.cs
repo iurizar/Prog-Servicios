6fd0998efeb25eec5135f83077552654a6f42438
iurizar <iurizar@outlook.com>
2019-10-03 08:46:29 +0200

    Mejora de operandos

---
+++ calculadoraProceso/calculadoraProceso/Program.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Diagnostics;
+using System.IO;
+using System.IO.Pipes;
+using System.Threading.Tasks;
+
+
+namespace calculadoraProceso
+{
+    class Program
+    {
+        private Process p1 = null;
+        static void Main(string[] args)
+        {
+            Process p;
+            ejecutaServidor(out p);
+            Task.Delay(1000).Wait();
+
+            var cliente = new NamedPipeClientStream("Pipe1");
+            cliente.Connect();
+            StreamReader reader = new StreamReader(cliente);
+            StreamWriter writer = new StreamWriter(cliente);
+
+            String entrada;
+            int opcion = 0;
+            do
+            {
+                Menu();
+                entrada = Console.ReadLine();
+                opcion = Int32.Parse(entrada);
+                if(opcion!=-1) 
+                { 
+                    writer.WriteLine(opcion);
+                    writer.Flush();
+                    Console.WriteLine("Introduzca el primer operando: ");
+                    var operando1 = Console.ReadLine();
+                    writer.WriteLine(operando1);
+                    writer.Flush();
+                    Console.WriteLine("Introduzca el segundo operando: ");
+                    var operando2 = Console.ReadLine();
+                    writer.WriteLine(operando2);
+                    writer.Flush();
+                    Console.WriteLine("Resultado : {0}", reader.ReadLine());
+                    writer.Flush();
+                }
+            } while (opcion != -1);
+
+
+        }
+
+        static Process ejecutaServidor(out Process p1)
+        {
+            // iniciar un proceso con el servidor y devolver
+            ProcessStartInfo info = new ProcessStartInfo(@"C:\Users\iuriz\source\repos\Prog-Servicios\calculadoraProceso\calculadoraServer\bin\Release\netcoreapp3.0\publish\calculadoraServidor.exe");
+            // su valor por defecto el false, si se establece a true no se "crea" ventana
+            info.CreateNoWindow = false;
+            info.WindowStyle = ProcessWindowStyle.Normal;
+            // indica si se utiliza el cmd para lanzar el proceso
+            info.UseShellExecute = true;
+            p1 = Process.Start(info);
+            return p1;
+        }
+
+        static void Menu()
+        {
+            Console.WriteLine("Operación a realizar:");
+            Console.WriteLine("1 Suma");
+            Console.WriteLine("2 Resta");
+            Console.WriteLine("3 Multiplicación");
+            Console.WriteLine("(-1) salir");
+        }
+    }
+}
+    
---
+++ calculadoraProceso/calculadoraServer/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.IO;
+using System.IO.Pipes;
+using System.Threading.Tasks;
+
+namespace calculadoraServidor
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                var server = new NamedPipeServerStream("Pipe1");
+                server.WaitForConnection();
+                Console.WriteLine("Servidor esperando datos");
+                StreamReader reader = new StreamReader(server);
+                StreamWriter writer = new StreamWriter(server);
+                int resultado = 0;
+                while (true)
+                {
+                    var entrada = reader.ReadLine();
+                    Console.WriteLine(String.Join("Se ha elegido la opcion: ", entrada));
+                    int opcion = int.Parse(entrada);
+                    Console.WriteLine("Ha elegido opción 1");
+                    var operando1 = reader.ReadLine();
+                    Console.WriteLine("Operando 1: " + operando1);
+                    var operando2 = reader.ReadLine();
+                    Console.WriteLine("Operando 2: " + operando2);
+                    int op1 = int.Parse(operando1);
+                    int op2 = int.Parse(operando2);
+                    switch (opcion)
+                    {
+                        case 1:
+                            resultado = op1 + op2;
+                            writer.WriteLine(resultado);
+                            writer.Flush();
+                            break;
+                        case 2:
+                            Console.WriteLine("Opción 2");
+                            break;
+                        case 3:
+                            Console.WriteLine("Opción 3");
+                            break;
+                        case -1:
+                            Console.WriteLine("Cerrando programa");
+                            break;
+                    }
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Error de conexión. Apagando servidor.");
+            }
+
+
+        }
+    }
+}
